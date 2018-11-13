666ae51402a05b3d5aed95a8f7d382127d1b8407
Matt Blanco <matthew.e.blanco@gmail.com>
2018-11-12 19:07:00 -0700

    Testing test code

--- bot.py
+++ bot.py
@@ -22,32 +22,20 @@
         # wait bot.delete_message(message)
 
 # bot.run('<YOUR_TOKEN_HERE>')
-
 import discord
-import asyncio
-import os
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-
-client.run(os.environ.get('TOKEN'))
+bot.run(os.environ.get('TOKEN'))
