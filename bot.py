d468e86aad2d7c2b8b9abe23abfb6c5f410ca070
Matt Blanco <matthew.e.blanco@gmail.com>
2018-11-13 15:49:07 -0700

    Fixed tabs to spaces

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ async def on_message(message):
     if message.channel.id == 511741087954501634:
         await client.send_message(message.channel, 'What the fuck')
     if message.content.startswith('!test'):
-		await client.send_message(message.channel, message.channel.id)
+        await client.send_message(message.channel, message.channel.id)
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
