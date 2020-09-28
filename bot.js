ee8d5ec8f7345805545ef4dcb1b52788fe8a361f
Matt Blanco <matthew.e.blanco@gmail.com>
2020-09-27 21:33:49 -0500

    changed captialization in message

--- bot.js
+++ bot.js
@@ -8,11 +8,11 @@ client.on('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith('!') && message.channel.name === 'jukebox' && !message.author.bot) {
         message.delete();
-        message.reply('Please use ' + message.guild.channels.find(channel => channel.name === "general").toString() + ' for general chat.');
+        message.reply('please use ' + message.guild.channels.find(channel => channel.name === "general").toString() + ' for general chat.');
     }
     else if (message.content.startsWith('!') && !message.author.bot) {
         message.delete();
-        message.reply('Please use ' + message.guild.channels.find(channel => channel.name === "jukebox").toString() + ' to play music.');
+        message.reply('please use ' + message.guild.channels.find(channel => channel.name === "jukebox").toString() + ' to play music.');
     }
 });
 
