8314955d7dfa4a6d8b8a59682b3cb008a64da71c
Matt Blanco <matthew.e.blanco@gmail.com>
2021-10-01 21:08:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,11 +9,11 @@ client.on('message', message => {
     if(message.author.bot){
         return;
     }
-else if (!message.content.startsWith('!') && !message.content.startsWith('--') && !message.content.startsWith('>>') && message.channel.name === 'jukebox') {
+    else if (!message.content.startsWith('!') && !message.content.startsWith('--') && !message.content.startsWith('>>') && !message.content.startsWith('.') && message.channel.name === 'jukebox') {
         message.delete();
         message.reply('please use ' + message.guild.channels.find(channel => channel.name === "general").toString() + ' for general chat.');
     }
-    else if ((message.content.startsWith('!') || message.content.startsWith('--') || message.content.startsWith('>>')) && message.channel.name !== 'jukebox') {
+    else if ((message.content.startsWith('!') || message.content.startsWith('--') || message.content.startsWith('>>')) || message.content.startsWith('.')) && message.channel.name !== 'jukebox') {
         message.delete();
         message.reply('please use ' + message.guild.channels.find(channel => channel.name === "jukebox").toString() + ' to play music.');
     }
